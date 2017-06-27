435ef89af5ddf773745c6162b379c0f54e94f439
Dane Kilpatrick <danekilpatrick@Danes-MacBook-Air.local>
2017-06-27 10:39:18 -0500

    Created a Bank_Accounts folder and finished the Checking/Savings Exercise

---
+++ bank_accounts/bank_accounts.py
@@ -0,0 +1,39 @@
+""" This is my bank accounts program """
+
+class SavingsAccount():
+    def __init__(self, kind):
+        self.kind = kind
+        self.balance = 0
+
+    def deposit(self, amount):
+        self. = capacity
+        self.amount = 100
+
+    def withdraw(self, amount):
+        self.capacity = capacity
+        self.amount = 0
+
+class CheckingAccount():
+    def __init__(self,kind)
+        self.kind = kind
+        self.balance = 0
+
+    def deposit(self, amount):
+        self.
+
+
+
+    class BankAccount():
+      def __init__(self, kind):
+        self.kind = kind
+        self.balance = 0
+        self.overdraft_fees = 0
+
+      def deposit(self, dep_amount):
+        self.balance += dep_amount
+
+      def withdraw(self, wit_amount):
+        self.balance -= wit_amount
+        if (self.balance < 0):
+          self.overdraft_fees += 20
+        return balance
